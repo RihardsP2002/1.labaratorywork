c65b3b291fdce687bb5f7a88f29c37792add51ea
Rihards <raligum123@inbox.lv>
2023-10-09 21:13:49 +0300

    Text view and button.

--- app/src/main/java/com/example/rihardspokulis1lab/MainActivity.kt
+++ app/src/main/java/com/example/rihardspokulis1lab/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.rihardspokulis1lab
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize views
+        val button1 = findViewById<Button>(R.id.button1)
+        val textView1 = findViewById<TextView>(R.id.textView1)
+
+        // Set OnClickListener for button1
+        button1.setOnClickListener {
+            // Change the text of textView1 when button1 is clicked
+            textView1.text = "Laboratory work 1" // Replace "New Text Here" with your desired text
+        }
     }
 }
+
+
+
+
+
+
